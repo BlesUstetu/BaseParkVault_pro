8ae4a89e6a5cf26896b30bc6233c7b8ed67b53bc
BlesUstetu <lionustetu@gmail.com>
2026-03-11 14:29:19 +0930

    Create BottomNav.jsx

---
+++ src/components/navigation/BottomNav.jsx
@@ -0,0 +1,16 @@
+export default function BottomNav(){
+
+return(
+
+<nav className="bottomNav">
+
+<button>Dashboard</button>
+<button>Vault</button>
+<button>History</button>
+<button>Analytics</button>
+
+</nav>
+
+)
+
+}
