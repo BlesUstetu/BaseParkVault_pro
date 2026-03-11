86203666a70b46bab05a6a1c985fd36b211f4c88
BlesUstetu <lionustetu@gmail.com>
2026-03-11 14:23:43 +0930

    Create History.jsx

---
+++ src/components/leaderboard/history/History.jsx
@@ -0,0 +1,22 @@
+export default function History(){
+
+return(
+
+<div className="history">
+
+<h3>Recent Transactions</h3>
+
+<div className="tx">
+<span>Deposit</span>
+<span>0.5 ETH</span>
+</div>
+
+<div className="tx">
+<span>Withdraw</span>
+<span>0.2 ETH</span>
+</div>
+
+</div>
+
+)
+}
