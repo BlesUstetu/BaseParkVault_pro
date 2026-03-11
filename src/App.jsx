3af4fd0dbf3ce7917ce6080ffebd47b9ee4cb1b2
BlesUstetu <lionustetu@gmail.com>
2026-03-11 14:13:46 +0930

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+import Header from "./components/Header"
+import Analytics from "./components/dashboard/Analytics"
+import PortfolioChart from "./components/charts/PortfolioChart"
+import VaultPanel from "./components/VaultPanel"
+import Leaderboard from "./components/leaderboard/Leaderboard"
+import History from "./components/history/History"
+import Toast from "./components/ui/Toast"
+import TxModal from "./components/ui/TxModal"
+import BottomNav from "./components/navigation/BottomNav"
+
+export default function App(){
+
+return(
+
+<div className="app">
+
+<Header/>
+
+<Analytics/>
+
+<PortfolioChart/>
+
+<VaultPanel/>
+
+<Leaderboard/>
+
+<History/>
+
+<TxModal/>
+
+<Toast/>
+
+<BottomNav/>
+
+</div>
+
+)
+
+}
